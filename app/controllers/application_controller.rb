3a71ad2e54441fc819cd2ffb85df36bec2376124
ckvvanle <phamvanle10dt2.bkdn@gmail.com>
2018-08-02 22:42:39 +0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
 	def hello
 		render html: "hello,world!"
-		
+
 	end
 	def goodbye
 		render html: "goodbye, see u a gaint!"
 	end
+  def goodnight
+    render html: "goodbye, see u a gaint!"
+  end
+  def good
+    render html: "goodbye, see u a gaint!"
+  end
 end
